5cda85edd92afdf3f13b021e4d27fa1e17f77a05
Shuflduf <shuflduf@gmail.com>
2025-01-03 02:22:17 -0700

    REPLIES WERE SURPRISLY EASY OH NO

--- routes/new_post.go
+++ routes/new_post.go
@@ -2,8 +2,10 @@ package routes
 
 import (
 	"cyansky/appwrite"
+	"fmt"
 	"net/http"
 
+	"github.com/appwrite/sdk-for-go/appwrite"
 	"github.com/appwrite/sdk-for-go/id"
 	"github.com/gin-gonic/gin"
 )
@@ -20,18 +22,34 @@ func MakePost(c *gin.Context) {
   database.RefreshServices()
 
 	content := reqBody["content"].(string)
-	author := reqBody["author"].(string)
+	// author := reqBody["author"].(string)
+  authorClient := appwrite.NewClient(
+    appwrite.WithEndpoint("https://cloud.appwrite.io/v1"),
+    appwrite.WithProject(database.ProjectId),
+    appwrite.WithSession(reqBody["token"].(string)),
+  )
+  service := appwrite.NewAccount(authorClient)
+  accountData, accErr := service.Get()
+  if accErr != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{
+      "error": "Failed to get account data",
+    })
+    return
+  }
+  authorAuthId := accountData.Id
+  authorData := database.GetUserData(authorAuthId)
+  author := authorData["$id"].(string)
+
+  newPostId := id.Unique()
   parent, isReply := reqBody["reply_to"].(string)
   if isReply {
-    // get prev replies 
     parentPost, err := database.DatabaseService.GetDocument("cyansky-main", "posts", parent)
     if err != nil {
       c.JSON(http.StatusInternalServerError, gin.H{
-        "error": "Failed to get parent post",
+        "error": fmt.Sprintf("Failed to get parent post: %s", err),
       })
       return
     }
-    replyPostId := id.Unique()
     var parentPostData map[string]interface{}
     err = parentPost.Decode(&parentPostData)
     if err != nil {
@@ -40,8 +58,8 @@ func MakePost(c *gin.Context) {
       })
       return
     }
-    parentPostReplies := parentPostData["replies"].([]string)
-    parentPostReplies = append(parentPostReplies, replyPostId)
+    parentPostReplies := parentPostData["replies"].([]interface{})
+    parentPostReplies = append(parentPostReplies, newPostId)
     // edit parent post to add reply 
     _, err = database.DatabaseService.UpdateDocument(
       "cyansky-main",
@@ -51,12 +69,18 @@ func MakePost(c *gin.Context) {
         "replies": parentPostReplies,
       }),
     )
+    if err != nil {
+      c.JSON(http.StatusInternalServerError, gin.H{
+        "error": fmt.Sprintf("Failed to update parent post: %s", err),
+      })
+      return
+    }
   }
 
 	_, err := database.DatabaseService.CreateDocument(
 		"cyansky-main",
 		"posts",
-		id.Unique(),
+		newPostId,
 		map[string]interface{}{
 			"content": content,
 			"author":  author,
